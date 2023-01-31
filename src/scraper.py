5099252e6442f0c3d9f985d9aee403fe87df3470
memo-13-byte <canmehmetoguz@gmail.com>
2023-01-31 16:52:02 +0300

    JSON is created and scraper.py is modified according to it.

--- src/scraper.py
+++ src/scraper.py
@@ -1,43 +1,47 @@
 from bs4 import BeautifulSoup, element, Tag
 import requests
-from typing import Optional
+from typing import Optional, Dict, List, Tuple
 
 
-def fetch_data_fromXML(todaysDate: str) -> tuple[list[str], str]:
-    possible_dates: list[str] = find_possible_dates(todaysDate)
-
-    r: requests.Response = requests.get(
-        'http://www.sksdb.hacettepe.edu.tr/YemekListesi.xml')
+def fetch_data_fromXML() -> Dict[str, Tuple[List[str], str]]:
+    r: requests.Response = requests.get('http://www.sksdb.hacettepe.edu.tr/YemekListesi.xml')
     r.encoding = 'utf-8'
     xml_text: str = r.text
-
+    dictionary_for_matching = {}
     soup: BeautifulSoup = BeautifulSoup(xml_text, 'xml')
 
     meals: list[str] = []
     calorie: str = ''
-
+    dates = []
+    days = []
+    calories = []
     day: element.Tag
     for day in soup.select('gun'):
         day_date_obj: Optional[Tag] = day.select_one('tarih')
-        assert day_date_obj is not None, "[ERROR] Can't get the date from the XML."
-        date: str = day_date_obj.text.split()[0]
+        day_date_object = day.select_one('tarih')
+        a = day_date_object.text.strip()
+        current_date_and_day = a.split("<p>")[0]
+        dates.append(current_date_and_day.split()[0])
+        days.append(current_date_and_day.split()[1])
 
-        if date in possible_dates:
-            meal_tag: element.Tag
-            for meal_tag in day.select('yemek'):
-                meal_str: str = meal_tag.text.strip()
+        meal_tag: element.Tag
+        for meal_tag in day.select('yemekler'):
+            meal_str: str = meal_tag.text.strip()
 
-                if meal_str:
-                    meals.append(meal_str)
-            calorie_select_obj: Optional[Tag] = day.select_one('kalori')
-            assert calorie_select_obj is not None, "[ERROR] Can't get total calorie amount from the XML."
-            calorie = calorie_select_obj.text
-
+            if meal_str:
+                meals.append(meal_str)
+        calorie_select_obj: Optional[Tag] = day.select_one('kalori')
+        assert calorie_select_obj is not None, "[ERROR] Can't get total calorie amount from the XML."
+        calorie = calorie_select_obj.text
+        calories.append(calorie)
     if not meals:
         # Maybe introduce proper error handling with logging?
-        print(f"[ERROR] There isn't a menu for the given date: {todaysDate}")
+        print(f"[ERROR] There isn't a menu in the XML: ")
 
-    return meals, calorie
+    for i in range(len(meals)):
+        meals[i] = [meals[i].split("\n")]
+        dictionary_for_matching[dates[i]] = meals[i], calories[i]
+    return dictionary_for_matching
 
 
 def find_possible_dates(_date: str) -> list[str]:
@@ -48,7 +52,7 @@ def find_possible_dates(_date: str) -> list[str]:
     2.  6.01.2023
     3.  6.1.2023
     4.  06.1.2023
-    
+
     Here I created a list that contains all of the possibilities. The website that is being
     scraped uses 6.01.2023, but they may not be decisive on this too (Remember the meal names).
 
@@ -66,3 +70,4 @@ def find_possible_dates(_date: str) -> list[str]:
     possible_dates.append(f'{day}.{int(month)}.{year}')
 
     return possible_dates
+
