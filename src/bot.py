ec586646e1b3e980e2678395ebf49b41d4e51bff
AenR <40569269+AenR@users.noreply.github.com>
2023-03-10 17:01:24 +0300

    database-fix

--- src/bot.py
+++ src/bot.py
@@ -146,9 +146,7 @@ def main() -> None:
     app.add_handler(CommandHandler("tomorrow", send_tomorrow))
     app.add_handler(CommandHandler("custom", send_custom))
     app.add_error_handler(err_handler)
-    
-    app.job_queue.run_once(update_db, 3)
-
+    app.job_queue.run_once(update_db, 3) #Updates db when program starts
     app.job_queue.run_daily(update_db, time=time(hour=UPDATE_DB_TIME_HOUR, minute=UPDATE_DB_TIME_MINUTE, tzinfo=tz))
     app.job_queue.run_daily(publish_menu_image, time=time(hour=SHARE_TIME_HOUR, minute=SHARE_TIME_MINUTE, tzinfo=tz))
     app.job_queue.run_daily(publish_menu_text, time=time(hour=SHARE_TIME_HOUR, minute=SHARE_TIME_MINUTE, tzinfo=tz))
