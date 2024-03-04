6cec647aabecbb7a9deb8b76668e02c9107ef92c
furkansimsekli <simseklifurkan0@gmail.com>
2024-03-04 21:36:33 +0300

    bot: fix max message length error for logging
    I fixed this issue before by splitting error messages in
    hu-announcement-bot. Until now I haven't seen an issue about this,
    but it's just a better error handler, no reason to not use it.

--- src/bot.py
+++ src/bot.py
@@ -99,9 +99,12 @@ async def send_tomorrow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 # Error Handler
 async def err_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
-    Below code belongs to python-telegram-bot examples.
+    Log the error and send a telegram message to notify the developer.
+
+    Below code belongs to python-telegram-bot examples, furkansimsekli "fixed" the 4096 character limit.
     Url: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/errorhandlerbot.py
     """
+
     # Log the error before we do anything else, so we can see it even if something breaks.
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
 
@@ -109,19 +112,31 @@ async def err_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> Non
     # list of strings rather than a single string, so we have to join them together.
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
     tb_string = ''.join(tb_list)
+    tb_msg = f"{html.escape(tb_string)}"
 
     # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096-character limit.
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    message = (
+    ctx_msg = (
         f"An exception was raised while handling an update\n"
-        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-        "</pre>\n\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}</pre>\n\n"
         f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
         f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-        f"<pre>{html.escape(tb_string)}</pre>"
     )
-    await context.bot.send_message(chat_id=LOGGER_CHAT_ID, text=message, parse_mode=telegram.constants.ParseMode.HTML)
+    await context.bot.send_message(chat_id=LOGGER_CHAT_ID, text=ctx_msg, parse_mode=telegram.constants.ParseMode.HTML)
+
+    if len(tb_msg) > 4096:
+        tb_msg_list = tb_msg.split("The above exception was the direct cause of the following exception:")
+
+        for tb_msg in tb_msg_list:
+            if len(tb_msg) > 4096:
+                await context.bot.send_message(chat_id=LOGGER_CHAT_ID, text=f"Traceback is too long!",
+                                               parse_mode=telegram.constants.ParseMode.HTML)
+            else:
+                await context.bot.send_message(chat_id=LOGGER_CHAT_ID, text=f"<pre>{tb_msg}</pre>",
+                                               parse_mode=telegram.constants.ParseMode.HTML)
+    else:
+        await context.bot.send_message(chat_id=LOGGER_CHAT_ID, text=f"<pre>{tb_msg}</pre>",
+                                       parse_mode=telegram.constants.ParseMode.HTML)
 
 
 # Jobs
