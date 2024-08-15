9d7eb04e813c2d7a240e1f87dde32636634b6cf7
furkansimsekli <simseklifurkan0@gmail.com>
2024-08-16 01:33:17 +0300

    config: use default values for certain variables
    Resolves #27

--- src/config.py
+++ src/config.py
@@ -27,22 +27,24 @@ TEXT_CHANNEL_ID: int = config["TEXT_CHANNEL_ID"]
 LOGGER_CHAT_ID: int = config["LOGGER_CHAT_ID"]
 
 # Time Configurations for menu sharing task
-SHARE_TIME_HOUR: int = config["SHARE_TIME_HOUR"]
-SHARE_TIME_MINUTE: int = config["SHARE_TIME_MINUTE"]
-UPDATE_DB_TIME_HOUR: int = config["UPDATE_DB_TIME_HOUR"]
-UPDATE_DB_TIME_MINUTE: int = config["UPDATE_DB_TIME_MINUTE"]
+SHARE_TIME_HOUR: int = config.get("SHARE_TIME_HOUR", 9)
+SHARE_TIME_MINUTE: int = config.get("SHARE_TIME_MINUTE", 15)
+UPDATE_DB_TIME_HOUR: int = config.get("UPDATE_DB_TIME_HOUR", 15)
+UPDATE_DB_TIME_MINUTE: int = config.get("UPDATE_DB_TIME_MINUTE", 0)
 
 # Polling or Webhook?
-WEBHOOK_CONNECTED: bool = config["WEBHOOK_CONNECTED"]
-PORT: int = config["PORT"]
-WEBHOOK_URL: str = config["WEBHOOK_URL"]
+WEBHOOK_CONNECTED: bool = config.get("WEBHOOK_CONNECTED", False)
+PORT: int = config.get("PORT", 51413)
+WEBHOOK_URL: str = config.get("WEBHOOK_URL", "") + "/" + TELEGRAM_API_KEY
 
 # Background Colors (list of Hex Numbers that represents colors)
-BACKGROUND_COLORS: list[str] = config["BACKGROUND_COLORS"]
+BACKGROUND_COLORS: list[str] = config.get("BACKGROUND_COLORS", ["#C9D6DF", "#F8F3D4", "#FFE2E2", "#E7D4B5", "#AEDEFC",
+                                                                "#EAFFD0", "#FFD3B4", "#BAC7A7", "#95E1D3", "#FCE38A",
+                                                                "#FFB4B4"])
 
 # Email Configuration
 SMTP_HOST = config["SMTP_HOST"]
-SMTP_PORT = config["SMTP_PORT"]
+SMTP_PORT = config.get("SMTP_PORT", 587)
 SMTP_USERNAME = config["SMTP_USERNAME"]
 SMTP_PASSWORD = config["SMTP_PASSWORD"]
 MAILING_LIST_ADDRESS = config["MAILING_LIST_ADDRESS"]
